30a7a9d358784e104e0dd4929b351b1264ee298c
ccpjobsnow <69213361+ccpjobsnow@users.noreply.github.com>
2024-02-11 22:05:46 -0300

    melhoria das annotations de validação e término dos 43 cenários de teste junto com os enums

--- src/main/java/com/ccp/vis/controller/VisResumeStep0DadosGeraisController.java
+++ src/main/java/com/ccp/vis/controller/VisResumeStep0DadosGeraisController.java
@@ -3,18 +3,13 @@ package com.ccp.vis.controller;
 import java.util.Map;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.ccp.fields.validations.annotations.ObjectText;
-import com.ccp.fields.validations.annotations.AllowedValues;
 import com.ccp.fields.validations.annotations.ValidationRules;
-import com.ccp.fields.validations.annotations.ObjectRules;
-import com.ccp.fields.validations.enums.AllowedValuesValidations;
-import com.ccp.fields.validations.enums.ObjectTextSizeValidations;
-import com.ccp.fields.validations.enums.ObjectValidations;
 
 @CrossOrigin
 @RestController
@@ -22,20 +17,14 @@ import com.ccp.fields.validations.enums.ObjectValidations;
 public class VisResumeStep0DadosGeraisController {
 
 	@PostMapping
-	@ValidationRules(simpleObjectRules = {
-		@ObjectRules(rule = ObjectValidations.requiredFields, fields = {"onlyHomeOffice", "ddds", "pcd", "disabilities", "companiesNotAllowed", "disponibility", "observations"}),
-		@ObjectRules(rule = ObjectValidations.nonRepeatedLists, fields = {"ddds","disabilities", "companiesNotAllowed"}),
-		@ObjectRules(rule = ObjectValidations.listFields, fields = {"ddds", "disabilities", "companiesNotAllowed"}),
-		@ObjectRules(rule = ObjectValidations.integerFields, fields = {"ddds","disponibility"}),
-		@ObjectRules(rule = ObjectValidations.booleanFields, fields = {"onlyHomeOffice","pcd"}),
-	},allowedValues = {
-			@AllowedValues(rule = AllowedValuesValidations.arrayWithAllowedNumbers , fields =  {"ddd"}, allowedValues = {"61", "62", "64", "65", "66", "67", "82", "71", "73", "74", "75", "77", "85", "88", "98", "99", "83", "81", "87", "86", "89", "84", "79", "68", "96", "92", "97", "91", "93", "94", "69", "95", "63", "27", "28", "31", "32", "33", "34", "35", "37", "38", "21", "22", "24", "11", "12", "13", "14", "15", "16", "17", "18", "19", "41", "42", "43", "44", "45", "46", "51", "53", "54	", "55", "47", "48", "49"}),
-			@AllowedValues(rule=AllowedValuesValidations.arrayWithAllowedTexts ,fields ={"disabilities"}, allowedValues = {"física/motora", "intelectual/mental", "visual", "auditiva"})
-	},objectTextsValidations =  {
-			@ObjectText(rule = ObjectTextSizeValidations.equalsOrLessThan, fields = {"observations"}, bound = 500)
-	}
-)
+	@ValidationRules(rulesClass = VisResumeStep0DadosGeraisValidations.class)
 	public Map<String, String> create(@RequestBody Map<String, Object> body){
+		return null;
+	}
+
+	@PatchMapping
+	@ValidationRules(rulesClass = VisResumeStep0DadosGeraisValidations.class)
+	public Map<String, String> update(@RequestBody Map<String, Object> body){
 		return null;
 	}
 	
