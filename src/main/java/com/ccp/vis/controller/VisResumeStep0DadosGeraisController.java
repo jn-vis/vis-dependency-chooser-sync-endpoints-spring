f8aa462dc483d74ce157714d10f2600a5c9f1900
ccpjobsnow <69213361+ccpjobsnow@users.noreply.github.com>
2024-02-20 22:57:07 -0300

    .

--- src/main/java/com/ccp/vis/controller/VisResumeStep0DadosGeraisController.java
+++ src/main/java/com/ccp/vis/controller/VisResumeStep0DadosGeraisController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ccp.validation.CcpJsonFieldsValidations;
-import com.ccp.vis.sync.validations.resumes.step0.VisResumeStep0DadosGeraisValidations;
+import com.ccp.vis.sync.validations.resumes.steps.VisResumeStep0DadosGeraisValidations;
 
 @CrossOrigin
 @RestController
