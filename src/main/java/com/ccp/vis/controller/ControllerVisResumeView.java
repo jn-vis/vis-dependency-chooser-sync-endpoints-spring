8edbc70156b6a22167189b0d1eb1c31ffddc5a8c
ccpjobsnow <69213361+ccpjobsnow@users.noreply.github.com>
2024-03-03 22:50:17 -0300

    .

---
+++ src/main/java/com/ccp/vis/controller/ControllerVisResumeView.java
@@ -0,0 +1,39 @@
+package com.ccp.vis.controller;
+
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ccp.decorators.CcpJsonRepresentation;
+import com.ccp.jn.vis.sync.service.SyncServiceVisPosition;
+import com.ccp.validation.CcpJsonFieldsValidations;
+import com.ccp.vis.sync.validations.JsonFieldsValidationsVisPosition;
+
+@CrossOrigin
+@RestController
+@RequestMapping(value = "recruiter/{email}/resumes/{resume}")
+public class ControllerVisResumeView {
+
+	private final SyncServiceVisPosition service = new SyncServiceVisPosition();
+	
+	@PostMapping
+	public Map<String, String> create(@PathVariable("email") String email, @RequestBody Map<String, Object> json){
+		CcpJsonFieldsValidations.validate(JsonFieldsValidationsVisPosition.class, json);
+		this.service.save(email, new CcpJsonRepresentation(json));
+		return null;
+	}
+
+	@PatchMapping
+	public Map<String, String> update(@PathVariable("email") String email, @RequestBody Map<String, Object> json){
+		CcpJsonFieldsValidations.validate(JsonFieldsValidationsVisPosition.class, json);
+		this.service.save(email, new CcpJsonRepresentation(json));
+		return null;
+	}
+	
+}
