3986bd255908ccaf631356be2e68630329dbcc87
ccpjobsnow <69213361+ccpjobsnow@users.noreply.github.com>
2024-05-15 21:57:25 -0300

    .

--- src/main/java/com/ccp/vis/controller/ControllerVisResume.java
+++ src/main/java/com/ccp/vis/controller/ControllerVisResume.java
@@ -3,15 +3,19 @@ package com.ccp.vis.controller;
 import java.util.Map;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ccp.constantes.CcpConstants;
 import com.ccp.decorators.CcpJsonRepresentation;
+import com.ccp.jn.sync.service.SyncServiceJnLogin;
 import com.ccp.jn.vis.sync.service.SyncServiceVisResume;
 import com.ccp.web.spring.utils.CcpSyncSessionValuesExtractor;
+import com.jn.vis.commons.utils.VisCommonsUtils;
 
 import jakarta.servlet.http.HttpServletRequest;
 
@@ -19,6 +23,8 @@ import jakarta.servlet.http.HttpServletRequest;
 @RestController
 @RequestMapping(value = "/resume/{email}")
 public class ControllerVisResume  implements CcpSyncSessionValuesExtractor{
+	
+	private final SyncServiceJnLogin loginService = new SyncServiceJnLogin();
 
 	private final SyncServiceVisResume service = new SyncServiceVisResume();
 
@@ -51,12 +57,31 @@ public class ControllerVisResume  implements CcpSyncSessionValuesExtractor{
 		return changeStatus;
 	}
 
-	public Map<String, Object> getResume(@PathVariable("email") String email, HttpServletRequest request){
+	public Map<String, Object> getData(@PathVariable("email") String email, HttpServletRequest request){
 		
 		CcpJsonRepresentation sessionValues = this.getSessionValues(request, email);
 		
-		Map<String, Object> changeStatus = this.service.getResume(sessionValues).content;
+		Map<String, Object> changeStatus = this.service.getData(sessionValues).content;
 	
 		return changeStatus;
+	}
+
+	@GetMapping("/type/{contentType}")
+	public Map<String, Object> getResumeFile(
+			@PathVariable("contentType") String contentType, 
+			@PathVariable("email") String email, 
+			HttpServletRequest request){
+		
+		CcpJsonRepresentation sessionValues = this.getSessionValues(request, email);
+		
+		this.loginService.validateSession(sessionValues);
+		
+		String resumeContent = VisCommonsUtils.getResumeContent(email, contentType);
+		
+		CcpJsonRepresentation put = CcpConstants.EMPTY_JSON
+				.put("content", resumeContent)
+				.put("type", contentType);
+		
+		return put.content;
 	}
 }
