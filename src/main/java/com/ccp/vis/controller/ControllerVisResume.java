3a46eaf0e8ce9f4a286d71cae37640b75e0e5583
ccpjobsnow <69213361+ccpjobsnow@users.noreply.github.com>
2024-05-18 02:22:57 -0300

    Separação dos filters de colocar dados da sessão (IP, token, useragent (navegador)), validação de sessão, validação de e-mail... modularização (por meio de filter) da lógica que preenche dados da sessão para todos os jsons de todos os endpoints

--- src/main/java/com/ccp/vis/controller/ControllerVisResume.java
+++ src/main/java/com/ccp/vis/controller/ControllerVisResume.java
@@ -3,6 +3,7 @@ package com.ccp.vis.controller;
 import java.util.Map;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,57 +11,52 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.ccp.constantes.CcpConstants;
 import com.ccp.decorators.CcpJsonRepresentation;
-import com.ccp.jn.sync.service.SyncServiceJnLogin;
 import com.ccp.jn.vis.sync.service.SyncServiceVisResume;
-import com.ccp.web.spring.utils.CcpSyncSessionValuesExtractor;
-import com.jn.vis.commons.utils.VisCommonsUtils;
-
-import jakarta.servlet.http.HttpServletRequest;
 
 @CrossOrigin
 @RestController
 @RequestMapping(value = "/resume/{email}")
-public class ControllerVisResume  implements CcpSyncSessionValuesExtractor{
+public class ControllerVisResume{
 	
-	private final SyncServiceJnLogin loginService = new SyncServiceJnLogin();
-
 	private final SyncServiceVisResume service = new SyncServiceVisResume();
 
 	@RequestMapping(method = {RequestMethod.POST, RequestMethod.PATCH})
-	public Map<String, Object> save(@PathVariable("email") String email, 
-			@RequestBody Map<String, Object> json, HttpServletRequest request
-			) {
-		CcpJsonRepresentation sessionValues = this.getSessionValues(request, email);
-		CcpJsonRepresentation resume = sessionValues.putAll(json);
-		Map<String, Object> save = this.service.save(resume).content;
+	public Map<String, Object> save(@RequestBody Map<String, Object> sessionValues) {
+
+		CcpJsonRepresentation values = new CcpJsonRepresentation(sessionValues);
+		
+		Map<String, Object> save = this.service.save(values).content;
+		
 		return save;
 	}
 
-	public Map<String, Object> delete(@PathVariable("email") String email, HttpServletRequest request){
+	@DeleteMapping
+	public Map<String, Object> delete(@RequestBody Map<String, Object> sessionValues){
 		
-		CcpJsonRepresentation sessionValues = this.getSessionValues(request, email);
-		
-		Map<String, Object> delete = this.service.delete(sessionValues).content;
+		CcpJsonRepresentation values = new CcpJsonRepresentation(sessionValues);
+
+		Map<String, Object> delete = this.service.delete(values).content;
 	
 		return delete;
 	}
 
-	public Map<String, Object> changeStatus(@PathVariable("email") String email, HttpServletRequest request){
+	@DeleteMapping("/status")
+	public Map<String, Object> changeStatus(@RequestBody Map<String, Object> sessionValues){
 		
-		CcpJsonRepresentation sessionValues = this.getSessionValues(request, email);
+		CcpJsonRepresentation values = new CcpJsonRepresentation(sessionValues);
 		
-		Map<String, Object> changeStatus = this.service.changeStatus(sessionValues).content;
+		Map<String, Object> changeStatus = this.service.changeStatus(values).content;
 	
 		return changeStatus;
 	}
 
-	public Map<String, Object> getData(@PathVariable("email") String email, HttpServletRequest request){
+	@GetMapping
+	public Map<String, Object> getData(@RequestBody Map<String, Object> sessionValues){
 		
-		CcpJsonRepresentation sessionValues = this.getSessionValues(request, email);
+		CcpJsonRepresentation values = new CcpJsonRepresentation(sessionValues);
 		
-		Map<String, Object> changeStatus = this.service.getData(sessionValues).content;
+		Map<String, Object> changeStatus = this.service.getData(values).content;
 	
 		return changeStatus;
 	}
@@ -68,18 +64,13 @@ public class ControllerVisResume  implements CcpSyncSessionValuesExtractor{
 	@GetMapping("/type/{contentType}")
 	public Map<String, Object> getResumeFile(
 			@PathVariable("contentType") String contentType, 
-			@PathVariable("email") String email, 
-			HttpServletRequest request){
+			@RequestBody Map<String, Object> sessionValues){
 		
-		CcpJsonRepresentation sessionValues = this.getSessionValues(request, email);
+		CcpJsonRepresentation values = new CcpJsonRepresentation(sessionValues).put("contentType", contentType);
 		
-		this.loginService.validateSession(sessionValues);
-		String resumeContent = VisCommonsUtils.getResumeContent(email, contentType);
-		
-		CcpJsonRepresentation put = CcpConstants.EMPTY_JSON
-				.put("content", resumeContent)
-				.put("type", contentType);
+		CcpJsonRepresentation put = this.service.getResumeFile(values);
 		
 		return put.content;
 	}
+
 }
