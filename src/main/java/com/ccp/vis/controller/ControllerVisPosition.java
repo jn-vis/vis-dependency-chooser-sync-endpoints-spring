7572a51b3b1002b4990c9e23e4eb9f5e40abdc66
ccpjobsnow <69213361+ccpjobsnow@users.noreply.github.com>
2024-05-30 01:45:22 -0300

    VisAsyncBusinessPositionSave

--- src/main/java/com/ccp/vis/controller/ControllerVisPosition.java
+++ src/main/java/com/ccp/vis/controller/ControllerVisPosition.java
@@ -1,6 +1,5 @@
 package com.ccp.vis.controller;
 
-import java.util.List;
 import java.util.Map;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -12,7 +11,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ccp.decorators.CcpJsonRepresentation;
@@ -86,25 +84,6 @@ public class ControllerVisPosition {
 				;
 		
 		Map<String, Object> result = this.service.getResumeContent(json).content;
-	
-		return result;
-	}
-	
-	@PostMapping("/resumes/sending/email")
-	public Map<String, Object> sendResumesToEmail(
-			@RequestParam("resumeIds") List<String> resumeIds,
-			@RequestParam("emails") List<String> emails,
-			@PathVariable("title") String title,
-			@RequestBody String sessionValues
-			){
-		
-		CcpJsonRepresentation json = new CcpJsonRepresentation(sessionValues)
-				.put("resumeIds", resumeIds)
-				.put("emails", emails)
-				.put("title", title)
-				;
-		
-		Map<String, Object> result = this.service.sendResumesToEmail(json).content;
 	
 		return result;
 	}
